a7d2f8c82a309f915a8536fb1fd2df06c6e0845e
Jeht99 <Jeht99@outlook.com>
2024-09-26 19:42:38 -0600

    [dataBase.py]: __createDb becomed into private method, __startConn and __endConn were added & Destructor created

--- dataBase.py
+++ dataBase.py
@@ -1,15 +1,16 @@
+from werkzeug.security import generate_password_hash
 import sqlite3
 import os
 #////////////////////////////////////////////////////////////////////////////////////////////////////
 class DataBase:
     def __init__(self) -> None:
         self.dbName = "dataBase.db"
-    #///////////////////////////////////////////////////////////////////////////////////////////////
-    def createDb(self) -> bool:
-        if os.path.isfile(self.dbName):
-            return False
-
-        conn = None
+        self.connection = self.__startConn()
+    #////////////////////////////////////////////////////////////////////////////////////////////////
+    def __del__(self) -> None:
+        self.__endConn()
+    #////////////////////////////////////////////////////////////////////////////////////////////////
+    def __createDb(self) -> bool:
         try:
             conn = sqlite3.connect(self.dbName)
             c = conn.cursor()
@@ -22,12 +23,29 @@ class DataBase:
             PRIMARY KEY("account_id" AUTOINCREMENT));'''
             
             c.execute(query)
-            conn.close()
         except:
+            platforms = {"nt": "del", "posix": "rm"} #Windows and Linux
             if conn:
                 conn.close()
-                os.system(f"del {self.dbName}")
+                os.system(f"{platforms[os.name]} {self.dbName}")
             return False
         
         return True
+    #////////////////////////////////////////////////////////////////////////////////////////////////
+    def __startConn(self)-> object:
+        if (os.path.isfile(self.dbName) == False and
+            self.__createDb() == False):
+            return None
+        
+        try:
+            conn = sqlite3.connect(self.dbName)
+        except:
+            return None
+        return conn
+    #////////////////////////////////////////////////////////////////////////////////////////////////
+    def __endConn(self)-> None:
+        if self.connection != None:
+            (self.connection).close()
+        self.connection == None
+    #////////////////////////////////////////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////////////////////////////////////
